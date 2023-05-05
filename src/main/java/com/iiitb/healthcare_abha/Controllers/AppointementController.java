c91c8db886493271f80b8657a6b6987a7338c30f
sai <vishnuvardhan67634@gmail.com>
2023-05-05 17:58:26 +0000

    updated responses to appointment

--- src/main/java/com/iiitb/healthcare_abha/Controllers/AppointementController.java
+++ src/main/java/com/iiitb/healthcare_abha/Controllers/AppointementController.java
@@ -3,12 +3,17 @@ package com.iiitb.healthcare_abha.Controllers;
 
 import com.iiitb.healthcare_abha.Entity.Appointements;
 import com.iiitb.healthcare_abha.Entity.Appointemnts_pks;
+import com.iiitb.healthcare_abha.Entity.DoctorHelper;
 import com.iiitb.healthcare_abha.Service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/appointment")
@@ -21,28 +26,48 @@ public class AppointementController {
 
 
     @GetMapping("/")
-   public List<Appointements> get_all_appointemnts(){
-      return  appointmentService.get_all_appointemnts();
+   public ResponseEntity<List<Appointements>> get_all_appointemnts(){
+       return ResponseEntity.ok().body(appointmentService.get_all_appointemnts());
 
 
     }
 
     @GetMapping("/create")
-    public String create_appointement(@RequestBody Appointements appointements){
+    public ResponseEntity<Map<String, Object>> create_appointement(@RequestBody Appointements appointements){
+        Map<String, Object> response = new HashMap<>();
+        appointmentService.create_appointement(appointements);
+        //if(s.equals("appointemnt created"))
 
-        return  appointmentService.create_appointement(appointements);
+            response.put("message", "Appointment created sucessfully");
 
+        return ResponseEntity.ok().body(response);
 
     }
 
     @PutMapping("/{pid}/{visitid}")
-    public String update_appointement(@PathVariable long pid, @PathVariable long visitid,@RequestBody Appointements appointements){
-        return appointmentService.update_appointement(pid, visitid, appointements);
+    public  ResponseEntity<Map<String, Object>> update_appointement(@PathVariable long pid, @PathVariable long visitid,@RequestBody Appointements appointements){
+        Map<String, Object> response = new HashMap<>();
+        if(appointmentService.update_appointement(pid, visitid, appointements).equals("there is no appointement of type to update")){
+            response.put("message","Appointment with " + pid +"not fount");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+        response.put("message", "Appointment with " + pid +"updated");
+
+        return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping ("/{pid}/{visitid}")
-    public String delete_appointement(@PathVariable long pid, @PathVariable long visitid){
-        return appointmentService.delete_appointement(pid, visitid);
+    public ResponseEntity<Map<String, Object>> delete_appointement(@PathVariable long pid, @PathVariable long visitid){
+        Map<String, Object> response = new HashMap<>();
+
+       if(appointmentService.delete_appointement(pid, visitid).equals("there is no appointement of type to delete")){
+           response.put("message","Appointment with " + pid +"not fount");
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+       }
+        //response.put("doctor", employee);
+        response.put("message", "Appointment with " + pid +"not found");
+
+        return ResponseEntity.ok().body(response);
     }
 
 
